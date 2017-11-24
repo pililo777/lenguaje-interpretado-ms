e69fd481189fd8cd8dc411b7a7fbc312caaaa24e
rrivero <rrivero@prakmatic.com>
2017-11-24 14:26:46 +0100

    Add project files.

---
+++ sql/sqldemo.c
@@ -0,0 +1,108 @@
+// sql.cpp : Defines the entry point for the console application.
+//
+
+//#include "stdafx.h"
+
+
+// SQLConnect_ref.cpp
+// compile with: odbc32.lib
+#include <windows.h>
+#include <sqlext.h>
+#include <stdio.h>
+#define _SQLNCLI_ODBC_
+#include "sqlncli.h"
+
+
+int mainsql2(void)
+{
+	SQLHENV henv;
+	SQLHDBC hdbc;
+	SQLHSTMT hstmt;
+	SQLRETURN retcode;
+	SQLPOINTER rgbValue;
+
+
+	SQLCHAR szText[200];
+	SQLWCHAR szCity[200];
+
+	SQLLEN cbText = 0;
+	SQLLEN cbCity = 0;
+
+	//	SQLINTEGER    cbText;
+
+	SQLCHAR OutConnStr[1024];
+	SQLSMALLINT OutConnStrLen;
+	HWND desktopHandle;
+
+	int i = 0;
+	rgbValue = &i;
+
+
+	desktopHandle = GetDesktopWindow();
+
+	// Allocate environment handle
+	retcode = SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &henv);
+
+	// Set the ODBC version environment attribute
+	if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+		retcode = SQLSetEnvAttr(henv, SQL_ATTR_ODBC_VERSION, (SQLPOINTER*)SQL_OV_ODBC3, 0);
+
+		// Allocate connection handle
+		if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+			retcode = SQLAllocHandle(SQL_HANDLE_DBC, henv, &hdbc);
+
+			// Set login timeout to 5 seconds
+			if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+				SQLSetConnectAttr(hdbc, SQL_LOGIN_TIMEOUT, (SQLPOINTER)(rgbValue), 0);
+				retcode = SQLDriverConnect(hdbc, desktopHandle, (SQLWCHAR*)L"Driver={SQL Server Native Client 11.0};Server=DESKTOP-SJA365E\\MSS10135;DataBase=Usuaris;Uid=sa;Pwd=1.Write.1;\0", 117, (SQLWCHAR*)OutConnStr, 255, &OutConnStrLen, SQL_DRIVER_COMPLETE);
+				// 117 es el largo de la connection string
+				if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+					retcode = SQLAllocHandle(SQL_HANDLE_STMT, hdbc, &hstmt);
+
+					retcode = SQLExecDirect(hstmt, (SQLWCHAR *)L"SELECT Top 10 Correu, Clau FROM Usuaris order by Clau Desc", SQL_NTS);
+					if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+						retcode = SQLBindCol(hstmt, 1, SQL_C_CHAR, szText, 200, &cbText);
+						retcode = SQLBindCol(hstmt, 2, SQL_C_CHAR, szCity, 200, &cbCity);
+
+
+						for (i; ; i++) {
+							retcode = SQLFetch(hstmt);    //COGE EL SIGUEINTE REGISTRO DE LA TABLA
+							if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO)
+								printf("%s -- %s \n", szText, szCity);
+							else
+								break;
+						}
+					}
+
+				}
+
+			}
+		}
+
+	}
+
+}
+
+//   https://stackoverflow.com/questions/15906580/how-to-configure-gtk-on-visual-studio-2010
+#include <gtk-2.0\gtk\gtk.h>
+
+int main_demo(int argc, char* argv[])
+{
+	mainsql();
+
+	gtk_init(&argc, &argv);
+
+	GtkWidget* window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_widget_set_usize(window, 300, 200);
+
+	g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+	gtk_window_set_title(GTK_WINDOW(window), "GTK+ with VS2010");
+
+	gtk_widget_show(window);
+
+	gtk_main();
+	return 0;
+}
+
+
+
